5118965e355acf9280f25a154cce68a43a26c255
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-27 12:42:31 +0100

    Update interpreter.py

--- interpreter.py
+++ interpreter.py
@@ -1,28 +1,4 @@
-#! /usr/bin/python
-
-# SPDX-License-Identifier: GPL-3.0-or-later
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
-"""This class implements a BASIC interpreter that
-presents a prompt to the user. The user may input
-program statements, list them and run the program.
-The program may also be saved to disk and loaded
-again.
-
-"""
-
+from colorama import init
 from basictoken import BASICToken as Token
 from lexer import Lexer
 from program import Program
@@ -33,20 +9,22 @@ def main():
 
     banner = (
         """
-         .d8888b.  888      d8b           .d88888b.   .d8888b.  
-        d88P  Y88b 888      Y8P          d88P   Y88b d88P  Y88b 
-        888    888 888                   888     888 Y88b.      
-        888        88888b.  888 88888b.  888     888  "Y888b.   
-        888        888  88b 888 888  88b 888     888     "Y88b. 
-        888    888 888  888 888 888  888 888     888        888 
-        Y88b  d88P 888  888 888 888 d88P Y88b. .d88P Y88b  d88P 
-          Y8888P   888  888 888 88888P     Y88888P     Y8888P
-                                888                             
-                                888                             
-                                888                             
+         ███████   ███      ███            ███████    ███████  
+        ████  ████ ███      ███          ████   ████ ████  ████
+        ███    ███ ███                   ███     ███ ████      
+        ███        ██████   ███ ██████   ███     ███  ███████   
+        ███        ███  ███ ███ ███  ███ ███     ███     █████ 
+        ███    ███ ███  ███ ███ ███  ███ ███     ███        ███
+        ████  ████ ███  ███ ███ ███ ████ ████   ████ ████  ████ 
+          ██████   ███  ███ ███ ██████     ███████     ██████
+                                ███                             
+                                ███                             
+                                ███                             
         """)
 
-    print(banner)
+    from colorama import Fore, Back, Style
+    print(Fore.MAGENTA + (banner))
+    print(Fore.RESET)
 
     lexer = Lexer()
     program = Program()
